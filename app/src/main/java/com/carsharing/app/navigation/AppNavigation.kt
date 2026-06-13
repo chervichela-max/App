4727e666c444402ad45c185d1afd10829d94410e
chervichela-max <chervichela@icloud.com>
2026-06-13 18:42:41 +0500

    Create AppNavigation.kt

---
+++ app/src/main/java/com/carsharing/app/navigation/AppNavigation.kt
@@ -0,0 +1,67 @@
+package com.carsharing.app.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.carsharing.app.ui.auth.PinScreen
+import com.carsharing.app.ui.car.CarScreen
+import com.carsharing.app.ui.main.MainScreen
+import com.carsharing.app.ui.more.MoreScreen
+
+sealed class Screen(val route: String) {
+    object Pin : Screen("pin")
+    object Main : Screen("main")
+    object Car : Screen("car")
+    object More : Screen("more")
+}
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    var isAuthenticated by remember { mutableStateOf(false) }
+    
+    NavHost(
+        navController = navController,
+        startDestination = if (isAuthenticated) Screen.Main.route else Screen.Pin.route
+    ) {
+        composable(Screen.Pin.route) {
+            PinScreen(
+                onSuccess = {
+                    isAuthenticated = true
+                    navController.navigate(Screen.Main.route) {
+                        popUpTo(Screen.Pin.route) { inclusive = true }
+                    }
+                }
+            )
+        }
+        
+        composable(Screen.Main.route) {
+            MainScreen(
+                onCarClick = { navController.navigate(Screen.Car.route) },
+                onServiceClick = { },
+                onMoreClick = { navController.navigate(Screen.More.route) }
+            )
+        }
+        
+        composable(Screen.Car.route) {
+            CarScreen()
+        }
+        
+        composable(Screen.More.route) {
+            MoreScreen(
+                onLogout = {
+                    isAuthenticated = false
+                    navController.navigate(Screen.Pin.route) {
+                        popUpTo(0) { inclusive = true }
+                    }
+                },
+                onPaymentClick = { },
+                onFinesClick = { },
+                onDebitClick = { }
+            )
+        }
+    }
+}
